3ea76c2ab667c17444861ec0054bcd94ede5600d
Miguel Ribeiro <miguel.rib.20@gmail.com>
2017-08-21 18:37:04 +0100

    Get info from url (if exists) or data attribute

--- scripts.js
+++ scripts.js
@@ -1,142 +1,164 @@
-var $window = $(window);
-var $document = $(document);
-var $pagesSpacer = $('.pagesSpacer');
-var $pagesContainer = $('.pagesContainer');
-var $pages = $('.pageSingle');
-var $header = $('.header');
+$(function () {
+	var $window = $(window);
+	var $document = $(document);
+	var $pagesSpacer = $('.pagesSpacer');
+	var $pagesContainer = $('.pagesContainer');
+	var $pages = $('.pageSingle');
+	var $header = $('.header');
 
-var html;
-var maxPageNumber = 3;
-var currentPage = 1;
-var pageCount = 1;
-var pageHeight = $pages.height();
-var screenHeight = $window.height();
-var pagePlaceholder = $header.height();
-var scrollValue;
-var prevScroll;
-var firstRun = true;
-var pagesViewport;
-var pagesObjHeight;
-var nextPageTriggerHeight;
-var prevPageTriggerHeight;
-var pagesCurrentHeight;
-var deletedPages = 0;
+	var html;
+	var maxPageNumber = $pagesContainer.data('maxPages');
+	var currentPage;
+	var pageCount = 1;
+	var pageHeight = $pages.height();
+	var screenHeight = $window.height();
+	var pagePlaceholder = $header.height();
+	var scrollValue;
+	var prevScroll;
+	var firstRun = true;
+	var pagesViewport;
+	var pagesObjHeight;
+	var nextPageTriggerHeight;
+	var prevPageTriggerHeight;
+	var pagesCurrentHeight;
+	var deletedPages = 0;
+	var startPage = 'startPage';
 
-var scrollAmount = {up: 0, down: 0};
+	var scrollAmount = {up: 0, down: 0};
 
-function loadPage(position, printPageNumber) {
-	html = '<div class="pageSingle">' + printPageNumber + '</div>';
-	if (position === 'ini') {
-		$pagesContainer.prepend(html);
-	} else if (position === 'end') {
-		$pagesContainer.append(html);
-	}
-	$pages = $('.pageSingle');
-}
-
-function loadNextPage() {
-	if (pagesViewport > nextPageTriggerHeight) {
-		pageCount++;
-		$('.pageCountValue').text(pageCount);
-		loadPage('end', pageCount);
-	}
-}
-
-function loadPrevPage() { // console.log('aqui');
-	if ((pagesViewport < prevPageTriggerHeight) && (deletedPages > 0)) {
-	// if (pagesViewport < prevPageTriggerHeight){
-		console.info('load prev page');
-		loadPage('ini', (currentPage - 1));
-		deletedPages--;
-		$('.deletedPageValue').text(deletedPages);
-		$pagesSpacer.height(deletedPages * pageHeight);
-	}
-}
-
-function removePage(position) {
-	if (position === 'first') {
-		
-		$pages.splice(0,1);
-		$('.pageSingle:first-child').remove();
-		deletedPages++;
-		$('.deletedPageValue').text(deletedPages);
-		$pagesSpacer.height(deletedPages * pageHeight);
-	} else if (position === 'last') {
-		// $pages.pop();
-		$pages.splice($pages.length-1,1);
-		$('.pageSingle:last-child').remove();
-		pageCount--;
-		$('.pageCountValue').text(pageCount);
-	}
-}
-
-function currentPageControl(direction) {
-	if (direction === 'down') {
-		if (pagesViewport > pagesCurrentHeight) {
-			currentPage++;
+	function loadPage(position, printPageNumber) {
+		html = '<div class="pageSingle">' + printPageNumber + '</div>';
+		if (position === 'ini') {
+			$pagesContainer.prepend(html);
+		} else if (position === 'end') {
+			$pagesContainer.append(html);
 		}
-	} else if (direction === 'up') {
-		if ((pagesViewport < (pagesCurrentHeight - pageHeight * 0.5)) && (currentPage > 1)) {
-			currentPage--;
+		$pages = $('.pageSingle');
+	}
+
+	function loadNextPage() {
+		if (pagesViewport > nextPageTriggerHeight) {
+			pageCount++;
+			$('.pageCountValue').text(pageCount);
+			loadPage('end', pageCount);
 		}
 	}
 
-	$('.currentPageValue').text(currentPage);
-}
-
-function onScroll() { console.log(currentPage);
-	scrollValue = $document.scrollTop();
-
-	pagesViewport = screenHeight - pagePlaceholder;
-	pagesObjHeight = pageHeight * pageCount;
-	// pagesObjHeight = pageHeight * currentPage;
-	pagesCurrentHeight = pageHeight * currentPage;
-
-	// screen half way through last visible page
-	nextPageTriggerHeight = pagesObjHeight - pageHeight / 2;
-
-	// screen half way through first visible page
-	prevPageTriggerHeight = $pagesSpacer.height() + pageHeight * 1.5;
-
-	// check if the screen is at half size
-	if ((pagesViewport > (pageHeight / 2)) && (firstRun)) {
-		pageCount++;
-		$('.pageCountValue').text(pageCount);
-		loadPage('end', pageCount);
-		firstRun = false;
-	}
-
-	if (prevScroll > scrollValue) { // scroll up
-		scrollAmount.up = scrollValue;
-
-		pagesViewport = pagesViewport + scrollValue;
-
-		currentPageControl('up');
-		loadPrevPage();
-
-		if ($pages.length > maxPageNumber) {
-			removePage('last');
-		}
-	} else if (prevScroll < scrollValue) { // scroll down
-		scrollAmount.down = scrollValue; console.log('aqui');
-
-		pagesViewport = pagesViewport + scrollValue;
-
-		currentPageControl('down');
-		loadNextPage();
-
-		if ($pages.length > maxPageNumber) {
-			removePage('first');
+	function loadPrevPage() { // console.log('aqui');
+		if ((pagesViewport < prevPageTriggerHeight) && (deletedPages > 0)) {
+		// if (pagesViewport < prevPageTriggerHeight){
+			console.info('load prev page');
+			loadPage('ini', (currentPage - 1));
+			deletedPages--;
+			$('.deletedPageValue').text(deletedPages);
+			$pagesSpacer.height(deletedPages * pageHeight);
 		}
 	}
 
-	// for visual aid (do not include)
-	$('.scrollDownValue').text(scrollAmount.down);
-	$('.scrollUpValue').text(scrollAmount.up);
+	function removePage(position) {
+		if (position === 'first') {
+			
+			$pages.splice(0,1);
+			$('.pageSingle:first-child').remove();
+			deletedPages++;
+			$('.deletedPageValue').text(deletedPages);
+			$pagesSpacer.height(deletedPages * pageHeight);
+		} else if (position === 'last') {
+			// $pages.pop();
+			$pages.splice($pages.length-1,1);
+			$('.pageSingle:last-child').remove();
+			pageCount--;
+			$('.pageCountValue').text(pageCount);
+		}
+	}
 
-	prevScroll = scrollValue;
-}
+	function currentPageControl(direction) {
+		if (direction === 'down') {
+			if (pagesViewport > pagesCurrentHeight) {
+				currentPage++;
+			}
+		} else if (direction === 'up') {
+			if ((pagesViewport < (pagesCurrentHeight - pageHeight * 0.5)) && (currentPage > 1)) {
+				currentPage--;
+			}
+		}
 
-onScroll();
-// $(window).on('scroll', _.debounce(onScroll, 50));
-$(window).on('scroll', onScroll);
+		$('.currentPageValue').text(currentPage);
+	}
+
+	function onScroll() { console.log(currentPage);
+		scrollValue = $document.scrollTop();
+
+		pagesViewport = screenHeight - pagePlaceholder;
+		pagesObjHeight = pageHeight * pageCount;
+		// pagesObjHeight = pageHeight * currentPage;
+		pagesCurrentHeight = pageHeight * currentPage;
+
+		// screen half way through last visible page
+		nextPageTriggerHeight = pagesObjHeight - pageHeight / 2;
+
+		// screen half way through first visible page
+		prevPageTriggerHeight = $pagesSpacer.height() + pageHeight * 1.5;
+
+		// check if the screen is at half size
+		if ((pagesViewport > (pageHeight / 2)) && (firstRun)) {
+			pageCount++;
+			$('.pageCountValue').text(pageCount);
+			loadPage('end', pageCount);
+			firstRun = false;
+		}
+
+		if (prevScroll > scrollValue) { // scroll up
+			scrollAmount.up = scrollValue;
+
+			pagesViewport = pagesViewport + scrollValue;
+
+			currentPageControl('up');
+			loadPrevPage();
+
+			if ($pages.length > maxPageNumber) {
+				removePage('last');
+			}
+		} else if (prevScroll < scrollValue) { // scroll down
+			scrollAmount.down = scrollValue;
+
+			pagesViewport = pagesViewport + scrollValue;
+
+			currentPageControl('down');
+			loadNextPage();
+
+			if ($pages.length > maxPageNumber) {
+				removePage('first');
+			}
+		}
+
+		// for visual aid (do not include)
+		$('.scrollDownValue').text(scrollAmount.down);
+		$('.scrollUpValue').text(scrollAmount.up);
+
+		prevScroll = scrollValue;
+	}
+
+	function init() {
+		var arr = $(location)
+					.attr('href')
+					.split('?');
+
+		if (arr.length === 1) {
+			currentPage = $pagesContainer.data('startPage');
+		} else {
+			arr[1]
+				.split('&')
+				.forEach(function (arg) {
+					if (arg.indexOf(startPage) !== -1) {
+						currentPage = arg.split('=')[1];
+					}
+				});
+		}
+	}
+
+	init();
+	onScroll();
+	// $(window).on('scroll', _.debounce(onScroll, 50));
+	$(window).on('scroll', onScroll);
+});
