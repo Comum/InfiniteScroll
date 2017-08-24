659603fe3b3b55d3f6466b55bd4c602aae9f0f05
Miguel Ribeiro <miguel.rib.20@gmail.com>
2017-08-24 15:57:45 +0100

    File cleanup

--- scripts.js
+++ scripts.js
@@ -1,226 +1,3 @@
-// $(function () {
-// 	var $window = $(window);
-// 	var $document = $(document);
-// 	var $pagesSpacer = $('.pagesSpacer');
-// 	var $pagesContainer = $('.pagesContainer');
-// 	var $pages = $('.pageSingle');
-// 	var $header = $('.header');
-
-// 	var html;
-// 	var maxPageNumber = $pagesContainer.data('maxPages');
-// 	var currentPage;
-// 	var pageCount = 0;
-// 	var pageHeight;
-// 	var screenHeight = $window.height();
-// 	var pagePlaceholder = $header.height();
-// 	var scrollValue;
-// 	var prevScroll = 0;
-// 	var firstRun = true;
-// 	var pagesViewport;
-// 	var pagesObjHeight;
-// 	var nextPageTriggerHeight;
-// 	var prevPageTriggerHeight;
-// 	var pagesCurrentHeight;
-// 	var deletedPages = 0;
-// 	var startPage = 'startPage';
-// 	var urlStartUp = false;
-
-// 	var scrollAmount = {up: 0, down: 0};
-
-// 	function loadPage(position, printPageNumber) {
-// 		html = '<div class="pageSingle">' + printPageNumber + '</div>';
-// 		if (position === 'ini') {
-// 			$pagesContainer.prepend(html);
-// 		} else if (position === 'end') {
-// 			$pagesContainer.append(html);
-// 		}
-// 		$pages = $('.pageSingle');
-// 	}
-
-// 	function loadNextPage() {
-// 		if (pagesViewport > nextPageTriggerHeight) {
-// 			pageCount++;
-// 			$('.pageCountValue').text(pageCount);
-// 			loadPage('end', pageCount);
-// 		}
-// 	}
-
-// 	function loadPrevPage() {
-// 		if ((pagesViewport < prevPageTriggerHeight) && (deletedPages > 0)) {
-// 			loadPage('ini', (currentPage - 1));
-// 			deletedPages--;
-// 			$('.deletedPageValue').text(deletedPages);
-// 			$pagesSpacer.height(deletedPages * pageHeight);
-// 		}
-// 	}
-
-// 	function removePage(position) {
-// 		if (position === 'first') {
-			
-// 			$pages.splice(0,1);
-// 			$('.pageSingle:first-child').remove();
-// 			deletedPages++;
-// 			$('.deletedPageValue').text(deletedPages);
-// 			$pagesSpacer.height(deletedPages * pageHeight);
-// 		} else if (position === 'last') {
-// 			$pages.splice($pages.lengths-1,1);
-// 			$('.pageSingle:last-child').remove();
-// 			pageCount--;
-// 			$('.pageCountValue').text(pageCount);
-// 		}
-// 	}
-
-// 	function currentPageControl(direction) {
-// 		if (direction === 'down') {
-// 			if (pagesViewport > pagesCurrentHeight) {
-// 				currentPage++;
-// 			}
-// 		} else if (direction === 'up') {
-// 			if (urlStartUp) {
-// 				// first load when the url is used
-// 				if ((pagesViewport < prevPageTriggerHeight) && (currentPage > 1)) {
-// 					currentPage--;
-// 					urlStartUp = false;
-// 				}
-// 			} else {
-// 				if ((pagesViewport < (pagesCurrentHeight - pageHeight * 0.5)) && (currentPage > 1)) {
-// 					currentPage--;
-// 				}
-// 			}
-// 		}
-
-// 		$('.currentPageValue').text(currentPage);
-// 	}
-
-// 	function scrollingOptions() {
-// 		if (prevScroll > scrollValue) { // scroll up
-// 			scrollAmount.up = scrollValue;
-
-// 			pagesViewport = pagesViewport + scrollValue;
-
-// 			currentPageControl('up');
-// 			loadPrevPage();
-
-// 			if ($pages.length > maxPageNumber) {
-// 				removePage('last');
-// 			}
-// 		} else if ((prevScroll < scrollValue) && (prevScroll !== 0)){ // scroll down
-// 			scrollAmount.down = scrollValue;
-
-// 			pagesViewport = pagesViewport + scrollValue;
-
-// 			currentPageControl('down');
-// 			loadNextPage();
-
-// 			if ($pages.length > maxPageNumber) {
-// 				removePage('first');
-// 			}
-// 		}
-// 	}
-
-// 	function updateValues() {
-// 		if (!pageHeight) {
-// 			pageHeight = $pages.height();
-// 		}
-// 		pagesViewport = screenHeight - pagePlaceholder;
-// 		pagesObjHeight = pageHeight * pageCount;
-// 		pagesCurrentHeight = pageHeight * currentPage;
-
-// 		// screen half way through last visible page
-// 		nextPageTriggerHeight = pagesObjHeight - pageHeight / 2;
-
-// 		// screen half way through first visible page
-// 		prevPageTriggerHeight = $pagesSpacer.height() + pageHeight * 1.5;
-// 	}
-
-// 	function onScroll() {
-// 		scrollValue = $document.scrollTop();
-
-// 		// check if the screen is at half size
-// 		if (firstRun) {
-// 			pageCount++;
-// 			loadPage('end', pageCount);
-// 			firstRun = false;
-// 			updateValues();
-
-// 			if (pagesViewport > (pageHeight / 2)) {
-// 				pageCount++;
-// 				$('.pageCountValue').text(pageCount);
-// 				loadPage('end', pageCount);
-// 			}
-// 		} else {
-// 			updateValues();
-// 			scrollingOptions();
-// 		}
-
-// 		// for visual aid (do not include)
-// 		$('.scrollDownValue').text(scrollAmount.down);
-// 		$('.scrollUpValue').text(scrollAmount.up);
-
-// 		prevScroll = scrollValue;
-// 	}
-
-// 	function init() {
-// 		var arr = $(location)
-// 					.attr('href')
-// 					.split('?');
-
-// 		if (arr.length === 1) {
-// 			currentPage = $pagesContainer.data('startPage');
-// 		} else {
-// 			urlStartUp = true;
-
-// 			arr[1]
-// 				.split('&')
-// 				.forEach(function (arg) {
-// 					if (arg.indexOf(startPage) !== -1) {
-// 						currentPage = parseInt(arg.split('=')[1], 10);
-// 					}
-// 				});
-
-// 			// don't load the first page normally
-// 			firstRun = false;
-			
-// 			if (currentPage > 1) {
-// 				loadPage('end', (currentPage - 1));
-// 			}
-// 			loadPage('end', currentPage);
-// 			pageHeight = $pages.height();
-
-// 			if (currentPage > 1) {
-// 				$pagesSpacer.height((currentPage - 1) * pageHeight);
-// 			}
-			
-// 			$('html, body').animate({
-// 				scrollTop: ($pagesContainer.offset().top + pageHeight)
-// 			}, 0);
-
-// 			pageCount = currentPage;
-// 			if (currentPage > 2) {
-// 				deletedPages = currentPage - 2;
-// 			}
-
-// 			// auxiliarty info
-// 			$('.deletedPageValue').text(deletedPages);
-// 			$('.pageCountValue').text(pageCount);
-// 			$('.currentPageValue').text(currentPage);
-// 		}
-// 	}
-
-// 	init();
-// 	onScroll();
-// 	// $(window).on('scroll', _.debounce(onScroll, 50));
-// 	$(window).on('scroll', onScroll);
-// });
-
-/**
- * jQuery plugin template by https://github.com/publicJorn
- * Features:
- * - dynamic plugin name (only supply once) so it's easy to change later
- * - plugin factory to make it work in the browser, or with AMD / COMMONJS modules
- * - Plugin instance is saved on the selector element
- * - Default options are saved to the instance in case you need to figure out a difference between passed options
- */
 (function(global, factory) {
 	'use strict';
 	if (typeof define === 'function' && define.amd) {
@@ -306,7 +83,7 @@
 			this.pagesCurrentHeight = 0;
 			this.nextPageTriggerHeight = 0;
 			this.prevPageTriggerHeight = 0;
-			this.$pages = this.$el.find('.pageSingle');
+			this.$pages = this.$el.find('.' + this.options.pageClassName);
 		}
 
 		initContainerOffset() {
@@ -318,17 +95,16 @@
 		}
 
 		loadPage(position, printPageNumber) {
-			var html = '<div class="pageSingle">' + printPageNumber + '</div>';
+			var html = '<div class="' + this.options.pageClassName + '">' + printPageNumber + '</div>';
 			if (position === 'ini') {
 				this.$el.prepend(html);
 			} else if (position === 'end') {
 				this.$el.append(html);
 			}
-			this.$pages = this.$el.find('.pageSingle');
+			this.$pages = this.$el.find('.' + this.options.pageClassName);
 		}
 
 		loadPrevPage() {
-// console.log(this.pagesViewport);
 			if ((this.pagesViewport < this.prevPageTriggerHeight) && (this.deletedPages > 0)) {
 				this.loadPage('ini', (this.currentPage - 1));
 				this.deletedPages--;
@@ -345,18 +121,15 @@
 
 		removePage(position) {
 			if (position === 'first') {
-				// this.$pages.splice(0,1);
-				$('.pageSingle:first-child').remove();
+				$('.' + this.options.pageClassName + ':first-child').remove();
 				this.deletedPages++;
-				// $pagesSpacer.height(deletedPages * pageHeight);
 				this.updateContainerPadding(this.deletedPages * this.options.pageHeight);
 			} else if (position === 'last') {
-				// this.$pages.splice($pages.lengths-1,1);
-				$('.pageSingle:last-child').remove();
+				$('.' + this.options.pageClassName + ':last-child').remove();
 				this.pageCount--;
 			}
 
-			this.$pages = this.$el.find('.pageSingle');
+			this.$pages = this.$el.find('.' + this.options.pageClassName);
 		}
 
 		urlHasStartPageInfo() {
