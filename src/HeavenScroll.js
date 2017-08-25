daef10c2376dcad68e924f6f827d2defbb87a3fd
Miguel Ribeiro <miguel.rib.20@gmail.com>
2017-08-25 11:35:15 +0100

    Changed to replaceState to allow to go back to previous page

--- src/HeavenScroll.js
+++ src/HeavenScroll.js
@@ -177,12 +177,11 @@ class HeavenScroll {
         return (query.length > 2 ? query + "&" : "?") + (newval ? param + "=" + newval : '');
     }
 
-    // change window.history to window.location.search
     urlQueryParamValueUpdate() {
         if ($(location).attr('href').split('?').length > 1) {
-            window.history.pushState("", "", this.replaceQueryParam(this.options.urlQueryParamName, this.currentPage, window.location.search));
+            window.history.replaceState("", "", this.replaceQueryParam(this.options.urlQueryParamName, this.currentPage, window.location.search));
         } else {
-            window.history.pushState("", "", '?' + this.options.urlQueryParamName + '=' + this.currentPage, window.location.search);
+            window.history.replaceState("", "", '?' + this.options.urlQueryParamName + '=' + this.currentPage, window.location.search);
         }
     }
 
